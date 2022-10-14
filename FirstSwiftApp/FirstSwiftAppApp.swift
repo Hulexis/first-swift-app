a9a05fc1553dba582ec0520a7301d72bfe84878e
Arne Johannesen <arne.johannesen00@gmail.com>
2022-10-14 19:35:31 +0200

    Initial Commit

---
+++ FirstSwiftApp/FirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftAppApp.swift
+//  FirstSwiftApp
+//
+//  Created by Arne Johannesen on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
